8abba1ff327e457fbe569914fcc10fb8f8d89f59
Ali <ayaanaleeay@gmail.com>
2024-03-13 08:20:58 +0500

    Written something

---
+++ script.js
@@ -0,0 +1,29 @@
+let arr = [1,2,3,4,5]
+let even = arr.filter(x => x%2==0)
+console.log(even)
+// arr.forEach( (val,index)=>{
+//     console.log(`${val} at index ${index}`)
+// } )
+// arr.map((val,index)=>{
+//         console.log(`${val} at index ${index}`)
+// })
+
+// let obj = {
+//     name: "pawan",
+//     age: 21
+//   };
+  
+//   let { name: name1, age: age1 } = obj;
+//   console.log(name1);
+//   console.log(age1);
+  
+//   let arr1 = [1,2,3,4,5]
+//   let arr2  = arr1
+//   let arr3 = [...arr1]
+//   console.log(arr1)
+//   console.log(arr2)
+//   console.log(arr3)
+//   console.log("changes")
+//   arr1[0]=11;
+//   console.log(arr2)
+//   console.log(`name ${name1}`)
